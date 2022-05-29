3ccbd0f6712d9f2714e038d2833efd79c64c1ca8
Linoy <linoy.glxs@gmail.com>
2022-05-29 12:28:08 +0300

    Added Share on Instagram option

--- gui.py
+++ gui.py
@@ -2,12 +2,15 @@
 import socket
 from tkinter import *
 from tkinter import filedialog
+from tkinter import messagebox
 import PIL.Image, PIL.ImageTk
 import time
 import cv2
 import pickle
 import struct
 import threading
+from instabot import Bot
+import ctypes
 
 class MyVideoCapture:
     def __init__(self, video_source, socket):
@@ -55,6 +58,89 @@ class MyVideoCapture:
         self.mutex.release()
 
 
+class ThreadWithException(threading.Thread):
+    def __init__(self, username, password, bot, grade):
+        threading.Thread.__init__(self)
+        self.username = username
+        self.password = password
+        self.bot = bot
+        self.grade = grade
+
+    def run(self):
+        self.bot.login(username=self.username, password=self.password)
+        self.bot.upload_photo("Picture2.png", caption="My Grade on SafetyGrade is " + self.grade +
+                                                      "!\nGet your garde too in SafetyGrade")
+
+    def get_id(self):
+        # returns id of the respective thread
+        if hasattr(self, '_thread_id'):
+            return self._thread_id
+        for id, thread in threading._active.items():
+            if thread is self:
+                return id
+
+    def raise_exception(self):
+        thread_id = self.get_id()
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id,
+                                                         ctypes.py_object(SystemExit))
+        if res > 1:
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
+            print('Exception raise failure')
+
+class Instagram:
+    def __init__(self, grade):
+        self.bot = Bot()
+        self.grade = grade
+        self.insta_screen = Tk()
+        self.insta_screen.title("Instagram")
+        self.insta_screen.iconbitmap('picture1_ZzW_icon.ico')
+        self.insta_screen.geometry("450x300")
+        self.login()
+
+    def login(self):
+        Label(self.insta_screen, text="\nLogIn To Instagram\n", font=("bold", 16)).pack()
+        Label(self.insta_screen, text="Username", font=("normal", 12)).pack()
+        self.username_login_entry = Entry(self.insta_screen, textvariable="username", font=("normal", 12))
+        self.username_login_entry.delete(0, END)
+        self.username_login_entry.pack()
+        Label(self.insta_screen, text="").pack()
+        Label(self.insta_screen, text="Password", font=("normal", 12)).pack()
+        self.password__login_entry = Entry(self.insta_screen, textvariable="password", show='*', font=("normal", 12))
+        self.password__login_entry.delete(0, END)
+        self.password__login_entry.pack()
+        Label(self.insta_screen, text="").pack()
+        button1 = Button(self.insta_screen, text="Login", width=10, height=2, font=("normal", 10), command=self.upload)
+        button1.pack()
+
+    def upload(self):
+        username = self.username_login_entry.get()
+        password = self.password__login_entry.get()
+        t1 = ThreadWithException(username, password, self.bot, self.grade)
+        t1.start()
+        start = time.time()
+        while True:
+            now = time.time()
+            if (now - start) > 2:
+                break
+        if t1.isAlive():
+            t1.raise_exception()
+            Label(self.insta_screen, text="error", font=('bold', 18), fg='red').pack()
+            time.sleep(5)
+        self.insta_screen.destroy()
+
+    def botaction(self, username, password):
+        self.bot.login(username=username, password=password)
+        self.bot.upload_photo("Picture2.png", caption="My Grade on SafetyGrade is " + self.grade +
+                                                      "!\nGet your garde too in SafetyGrade")
+        self.insta_screen.destroy()
+
+    def manage_time(self):
+        start = time.time()
+        while True:
+            now = time.time()
+            if (now - start) > 2:
+                break
+
 class GUI:
     def __init__(self):
         self.username = ''
@@ -63,7 +149,10 @@ class GUI:
         self.port = 1234
         self.ip = '127.0.0.1'
         self.s = socket.socket()
-        self.s.connect((self.ip, self.port))
+        try:
+            self.s.connect((self.ip, self.port))
+        except:
+            messagebox.showerror("Error", "Error message")
         while True:
             data = self.s.recv(1024).decode()
             if data:
@@ -212,51 +301,92 @@ class GUI:
         print('Selected:', filename)
         self.file = filename
         self.uploadbtn.destroy()
+        self.destroyobjects = []
         self.vid = MyVideoCapture(self.file, self.s)
         self.canvas = Canvas(self.screen, width=self.vid.width, height=self.vid.height, bg=self.color)
-        self.stop_btn = Button(self.screen, text="Stop Video", width=15, height=1, bg='#567', fg='White', font=("normal", 18))
-        self.stop_btn.bind("<Button-1>", self.finish)
-        self.stop_btn.pack()
+        stop_btn = Button(self.screen, text="Stop Video", width=15, height=1, bg='#567', fg='White', font=("normal", 18))
+        stop_btn.pack()
+        stop_btn.bind("<Button-1>", self.finish)
+        self.destroyobjects.append(stop_btn)
+        l = Label(self.screen, text="\nSpeed: ", font=('normal', 15), bg=self.color)
+        l.pack()
+        self.destroyobjects.append(l)
+        self.buttons = Frame(self.screen, bg=self.color, width=300, height=300)
+        self.buttons.pack(side="top", expand=True, fill="both")
+        self.destroyobjects.append(self.buttons)
+        b05 = Button(self.buttons, text="0.5", width=10, height=1, bg='#567', fg='White', font=("normal", 12),
+                     command=self.speed05)
+        b1 = Button(self.buttons, text="1", width=10, height=1, bg='#567', fg='White', font=("normal", 12),
+                    command=self.speed1)
+        b15 = Button(self.buttons, text="1.5", width=10, height=1, bg='#567', fg='White', font=("normal", 12),
+                     command=self.speed15)
+        l = Label(self.buttons, text=('                '*3), bg=self.color, font=('normal', 20))
+        l.grid(row=0, column=0)
+        b05.grid(row=0, column=1)
+        b1.grid(row=0, column=2)
+        b15.grid(row=0, column=3)
+        self.destroyobjects.append(l)
+        self.destroyobjects.append(b05)
+        self.destroyobjects.append(b1)
+        self.destroyobjects.append(b15)
         self.sign = StringVar()
         self.sign.set('')
         self.signlabel = Label(self.screen, textvariable=self.sign, font=("normal", 18), fg='red', bg=self.color)
         self.signlabel.pack()
+        self.destroyobjects.append(self.signlabel)
         self.canvas.pack()
-        self.screen.after(10, self.update)
+        self.destroyobjects.append(self.canvas)
+        self.sec = 10
+        self.screen.after(self.sec, self.update)
+
+    def speed05(self):
+        self.sec = 30
+
+    def speed1(self):
+        self.sec = 10
+
+    def speed15(self):
+        self.sec = 3
 
     def finish(self, event):
-        self.stop_btn.destroy()
-        self.signlabel.destroy()
-        self.canvas.destroy()
+        for w in self.destroyobjects:
+            w.destroy()
         self.vid.delete()
         self.screen.after_cancel(self.after)
         self.s.setblocking(True)
-        #self.s.send('finish'.encode())
         self.grade = self.s.recv(1024).decode()
         self.gradePage()
 
     def gradePage(self):
         print(self.grade)
         if ',' in self.grade:
-            current, previous = self.grade.split(',', 1)
+            self.current, previous = self.grade.split(',', 1)
         else:
-            current = int(self.grade)
+            self.current = int(self.grade)
             previous = ''
-        Label(self.screen, text="Your Grade:", font=('normal', 20), bg=self.color).pack()
-        gradelbl = Label(self.screen, text=str(current), fg='red')
-        gradelbl.config(font=("Courier", 30), bg=self.color)
-        gradelbl.pack()
+        Label(self.screen, text="Your Grade:", font=('normal', 20), bg=self.color).pack(side=TOP)
+        gradelbl = Label(self.screen, text=str(self.current), fg='red', font=("Courier", 30), bg=self.color)
+        gradelbl.pack(side=TOP)
         if previous != '':
             Label(self.screen, text="Your previous grades:", font=('normal', 18), bg=self.color).pack()
             gradelbl = Label(self.screen, text=previous, fg='red', font=("normal", 16), bg=self.color)
-            gradelbl.pack()
-        Label(self.screen, text='', font=("normal", 16), bg=self.color).pack()
-        button1 = Button(self.screen, text="Logout", width=20, height=2, command=self.welcomePage, bg='#567', fg='White', font=("normal", 14))
-        button1.pack()
-        button1 = Button(self.screen, text="upload another video", width=20, height=2, bg='#567', fg='White', font=("normal", 14))
+            gradelbl.pack(side=TOP)
+        Label(self.screen, text='', font=("normal", 16), bg=self.color).pack(side=TOP)
+        button1 = Button(self.screen, text="Logout", width=20, height=2, command=self.welcomePage, bg='#567', fg='White'
+                         , font=("normal", 14))
+        button1.pack(side=TOP)
+        button1 = Button(self.screen, text="Upload Another video", width=20, height=2, bg='#567', fg='White',
+                         font=("normal", 14))
         button1.bind("<Button-1>", self.video)
-        button1.pack()
+        button1.pack(side=TOP)
+        button1 = Button(self.screen, text="Share On Instagram", width=20, height=2, bg='#567', fg='White',
+                         font=("normal", 14))
+        button1.bind("<Button-1>", self.instagram)
+        button1.pack(side=TOP)
         self.screen.mainloop()
+
+    def instagram(self, event):
+        instagram = Instagram(str(self.current))
 
     def update(self):
         ret, frame, sign = self.vid.get_frame()
@@ -268,7 +398,7 @@ class GUI:
                     sign = sign.split(')')[0]
                 self.sign.set(sign)
                 self.screen.update_idletasks()
-        self.after = self.screen.after(10, self.update)
+        self.after = self.screen.after(self.sec, self.update)
 
 
 if __name__ == '__main__':
